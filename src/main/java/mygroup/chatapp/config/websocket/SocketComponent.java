113ebd1f23d2e8064670e9fedbcaa0b2e6cd228c
adenita <nittaluta@gmail.com>
2024-03-19 00:19:59 +0100

    add SocketComponent

---
+++ src/main/java/mygroup/chatapp/config/websocket/SocketComponent.java
@@ -0,0 +1,54 @@
+package mygroup.chatapp.config.websocket;
+
+import com.corundumstudio.socketio.SocketIOServer;
+import com.corundumstudio.socketio.listener.ConnectListener;
+import com.corundumstudio.socketio.listener.DataListener;
+import com.corundumstudio.socketio.listener.DisconnectListener;
+import mygroup.chatapp.message.services.impl.SocketServiceImpl;
+import mygroup.chatapp.message.transports.MessageTransport;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class SocketComponent {
+    private final SocketServiceImpl socketService;
+
+    public SocketComponent(@Lazy SocketIOServer server, SocketServiceImpl socketService) {
+        this.socketService = socketService;
+        server.addConnectListener(this.onConnected());
+        server.addDisconnectListener(this.onDisconnected());
+        server.addEventListener("send_message", MessageTransport.class, this.onChatReceived());
+    }
+
+    private DataListener<MessageTransport> onChatReceived() {
+        return (senderClient, data, ackSender) -> {
+            socketService.saveAndBroadcastMessage(senderClient, data);
+        };
+    }
+
+    private ConnectListener onConnected() {
+        return (client) -> {
+            var params = client.getHandshakeData().getUrlParams();
+
+            String room = "";
+            if (params.containsKey("room") && params.get("room") != null) {
+                room = String.join("", params.get("room"));
+                client.joinRoom(room);
+            }
+
+        };
+
+    }
+
+    private DisconnectListener onDisconnected() {
+        return client -> {
+            var params = client.getHandshakeData().getUrlParams();
+            String room = "";
+            if (params.containsKey("room") && params.get("room") != null) {
+                room = String.join("", params.get("room"));
+                client.leaveRoom(room);
+            }
+        };
+    }
+}
