7140994ef1e6aaca26d3c8a2786a48fdc319dd43
adenita <nittaluta@gmail.com>
2024-03-19 01:19:42 +0100

    added findByUsername method

--- src/main/java/mygroup/chatapp/user/repositories/UserRepository.java
+++ src/main/java/mygroup/chatapp/user/repositories/UserRepository.java
@@ -6,9 +6,13 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     @Query(value="select * from room_users where room_id = :roomId", nativeQuery=true)
     List<User> getRoomUsers(Long roomId);
+
+    @Query(value = "select * from users where username = :username", nativeQuery = true)
+    Optional<User> findByUsername(String username);
 }
