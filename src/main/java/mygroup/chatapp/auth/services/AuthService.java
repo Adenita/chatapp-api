647887c30db1b419048a5f3ebc5f59e46e8f6e1f
adenita <nittaluta@gmail.com>
2024-03-19 01:20:45 +0100

    removed unused imports

--- src/main/java/mygroup/chatapp/auth/services/AuthService.java
+++ src/main/java/mygroup/chatapp/auth/services/AuthService.java
@@ -9,13 +9,8 @@ import mygroup.chatapp.user.entities.User;
 import mygroup.chatapp.user.repositories.UserRepository;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import java.util.Collection;
 
 @Service
 @RequiredArgsConstructor
@@ -46,11 +41,6 @@ public class AuthService {
         );
 
         var user = userRepository.findByUsername(loginTransport.getUsername()).orElseThrow();
-
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-
-        @SuppressWarnings("unchecked")
-        Collection<SimpleGrantedAuthority> list = (Collection<SimpleGrantedAuthority>) auth.getAuthorities();
 
         var jwtToken = jwtService.generateToken(user);
         return AuthTransport.builder()
